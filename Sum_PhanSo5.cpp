5a5e1025fe82d3da274e94c6a5becf8a969510d7
huulong1412 <huulong1412@gmail.com>
2025-04-19 12:42:25 +0700

    Initial commit

---
+++ Sum_PhanSo5.cpp
@@ -0,0 +1,33 @@
+/*
+    S(n) = 1/2 + 2/3 + 3/4 + ... + n/(n+1)
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, n;
+    float S;
+    S = 0;
+    i = 1;
+    do
+    {
+        printf("\nNhap n: ");
+        scanf("%d", &n);
+        if(n < 1)
+        {
+            printf("\nN phai lon hon hoac bang 1. Xin nhap lai !");
+        }
+    }while(n < 1);
+
+    while(i <= n)
+    {
+        S = S + (float)i / (i + 1);
+        i++;
+    }
+    printf("\nTong la %f", S);
+
+    getch();
+    return 0;
+}
