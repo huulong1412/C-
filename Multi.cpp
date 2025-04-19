934c822a16bc1820ffc6c09f6b26a954dce80f3a
huulong1412 <huulong1412@gmail.com>
2025-04-19 12:57:33 +0700

    Initial commit

---
+++ Multi.cpp
@@ -0,0 +1,33 @@
+/*
+ T(n) = 1 𝑥 2 𝑥 3 𝑥 … . 𝑥 𝑛.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, n;
+    long P;
+    i = 1;
+    P = 1;
+    do
+    {
+        printf("\nNhap n: ");
+        scanf("%d", &n);
+        if(n < 1)
+        {
+            printf("\nN phai lon hon hoac bang 1. Xin nhap lai !");
+        }
+    }while(n < 1);
+
+    while(i < n)
+    {
+        P = P * i;
+        i++;
+    }
+    printf("\nTich 1x2x....x%d la: %ld", i, P);
+
+    getch();
+    return 0;
+}
