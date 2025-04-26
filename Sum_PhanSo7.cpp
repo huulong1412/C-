cc9dbe4a9fbb4ee957b8ea10bbfaff4b3578e1ea
huulong1412 <huulong1412@gmail.com>
2025-04-26 13:03:02 +0700

    Initial commit

---
+++ Sum_PhanSo7.cpp
@@ -0,0 +1,36 @@
+/* 
+Tính S(n) = 1+ 1/(1+2) + 1/(1+2+3) + 1/(1+2+3+4) + ... + 1/(1+2+3+...+n)
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, n;
+    float S, T;
+    i = 1;
+    S = 0;
+    T = 0;
+    do
+    {
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+      if(n < 1)
+       {
+        printf("\nN phai >= 1. Xin Nhap lai!");
+       }
+    }while(n < 1);
+
+    while(i <= n)
+    {
+        T = T + i;
+        S = S + 1.0 / T;
+        i++;
+    }
+    printf("\nTong la %f", S);
+
+    getch();
+    return 0;
+}
