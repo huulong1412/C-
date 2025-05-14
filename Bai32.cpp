189f02689d43fa7d188b3d2c7753394e88c7cd78
huulong1412 <huulong1412@gmail.com>
2025-05-14 14:21:26 +0700

    Initial commit

---
+++ Bai32.cpp
@@ -0,0 +1,32 @@
+/*
+  Cho số nguyên dương n. Kiểm tra số nguyên dương n có phải là số
+chính phương hay không?
+Số chính phương là số mà kết quả khai căn bậc 2 là 1 số nguyên*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int i, n;
+    do
+    {
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+
+       if(n <= 0)
+           printf("\nn phai > 0. Xin nhap lai !");
+
+    }while(n <= 0);
+    
+    if (sqrt((float)n) == (int)sqrt((float)n))  // So sánh 2 số khi chưa ép về kiểu nguyên và số đã ép về kiểu nguyên
+    {
+        printf("\n%d La so chinh phuong", n);
+    }
+    else
+    {
+        printf("\n%d Khong la so chinh phuong", n);
+    }
+
+    getch();
+    return 0;
+}
