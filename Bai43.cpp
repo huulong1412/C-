1d957fe8e0f7724542c41db3f606e619c7807cb8
huulong1412 <huulong1412@gmail.com>
2025-05-23 12:39:05 +0700

    Initial commit

---
+++ Bai43.cpp
@@ -0,0 +1,30 @@
+/*
+  Tính tổng các chữ số của số nguyên dương n
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int themang, n;
+    int S;
+
+    do
+    {
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    }while(n < 0 && printf("\nLoi: n >= 0 !"));
+    S = 0;
+    themang = n;
+
+    while(themang != 0)
+    {
+        S = S + themang % 10;
+        themang = themang / 10;
+    }
+    printf("\nTong la %d", S);
+
+    getch();
+    return 0;
+}
