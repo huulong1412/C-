2e58cf4c5e885b1740df234cd229c274717dacc0
huulong1412 <huulong1412@gmail.com>
2025-05-24 15:49:44 +0700

    Initial commit

---
+++ Bai46.cpp
@@ -0,0 +1,37 @@
+/*
+  Hãy đếm số lượng chữ số lẻ của số nguyên dương n
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    long themang, n;
+    int sochuso;
+
+    do
+    {
+    printf("\nNhap n: ");
+    scanf("%ld", &n);
+    }while(n < 0 && printf("\nLoi: n >= 0 !"));
+    sochuso = 0;
+    themang = n;
+
+    if(n == 0)
+            sochuso = 1;
+    while(themang != 0)
+    {
+        if(themang % 2 == 1)
+        {
+        sochuso = sochuso + 1;
+        }
+        themang = themang / 10;
+    }
+    printf("\nSo chu so le cua %ld la %d", n, sochuso);
+
+    
+
+    getch();
+    return 0;
+}
