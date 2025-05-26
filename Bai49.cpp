b5247330a303420f04ee92217c231bad28a7a204
huulong1412 <huulong1412@gmail.com>
2025-05-26 12:01:13 +0700

    Initial commit

---
+++ Bai49.cpp
@@ -0,0 +1,36 @@
+/*
+  Cho số nguyên dương n. Hãy tìm chữ số đầu tiên của n
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int themang, n, i;
+
+    do
+    {
+        printf("\nNhap n: ");
+        scanf("%d", &n);
+    }while(n < 0 && printf("\nLoi: (n >= 0)"));
+
+    themang = n;
+    /*do
+    {                                 
+        i = themang % 10;
+        themang /= 10;
+    }while(themang != 0);*/                
+    
+    //có thể viết 
+    do
+    {
+      i = themang % 10;
+    }while(themang /= 10);
+    
+    printf("\nChu so dau tien la %d", i);
+
+
+    getch();
+    return 0;
+}
