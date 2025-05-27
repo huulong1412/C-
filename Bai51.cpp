7ed398f4749f07bd28e9f11846f2cbc1705dee0e
huulong1412 <huulong1412@gmail.com>
2025-05-27 09:32:09 +0700

    Initial commit

---
+++ Bai51.cpp
@@ -0,0 +1,35 @@
+/*
+  Tìm chữ số lớn nhất của số nguyên dương n
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int i, n;
+    int max;
+    max = 0;
+    do
+    {
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    }while(n < 0 && printf("\nLoi: n >= 0 !"));
+    if(n == 0)
+        max = 0;
+
+    do
+    {
+        i = n % 10;
+       if(i > max)
+       {
+           max = i;
+       }
+    }while(n /= 10);
+
+    printf("\nChu so lon nhat la %d", max);
+
+    getch();
+    return 0;
+}
