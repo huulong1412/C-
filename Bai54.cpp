897cdc189659a87656c22a864b76e699401e5449
huulong1412 <huulong1412@gmail.com>
2025-05-27 09:44:21 +0700

    Initial commit

---
+++ Bai54.cpp
@@ -0,0 +1,37 @@
+/*
+  Hãy đếm số lượng chữ số nhỏ nhất của số nguyên dương n
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int i, n;
+    int min, dem;
+    dem = 0;
+    do
+    {
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    }while(n < 0 && printf("\nLoi: n >= 0 !"));
+    min = n % 10;  // khởi tạo min
+    if(n == 0)
+        min = 0;
+
+    do
+    {
+        i = n % 10;
+        if(i == min)
+            dem++;
+       if(i < min)
+           min = i;
+    }while(n /= 10);
+
+    printf("\nChu so nho nhat la %d", min);
+    printf("\nSo luong chu so nho nhat la %d", dem);
+
+    getch();
+    return 0;
+}
