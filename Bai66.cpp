7bab227367a00a6ae776e12afe5a3acf7c78618e
huulong1412 <huulong1412@gmail.com>
2025-06-03 13:10:16 +0700

    Initial commit

---
+++ Bai66.cpp
@@ -0,0 +1,42 @@
+/* Giải phương trình 𝑎𝑥^4 + 𝑏𝑥^2 + 𝑐 = 0 */
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c;
+
+    do {
+        printf("\nNhap vao so thuc a (a != 0) = ");
+        scanf("%f", &a);
+        if (a == 0) {
+            printf("\nGia tri nhap vao khong hop le. Xin kiem tra lai !");
+        }
+    } while (a == 0);
+
+    printf("\nNhap vao so thuc b = ");
+    scanf("%f", &b);
+
+    printf("\nNhap vao so thuc c = ");
+    scanf("%f", &c);
+
+    // Tính delta
+    float delta = b * b - 4 * a * c;
+
+    if (delta < 0) {
+        printf("\nPhuong trinh vo nghiem.");
+    } else if (delta == 0) {
+        // Nghiệm kép
+        float x = -b / (2 * a);
+        printf("\nPhuong trinh co nghiem kep: x1 = x2 = %.2f", x);
+    } else {
+        // Hai nghiệm phân biệt
+        float x1 = (-b + sqrt(delta)) / (2 * a);
+        float x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("\nPhuong trinh co hai nghiem phan biet: x1 = %.2f, x2 = %.2f", x1, x2);
+    }
+
+    getch();
+    return 0;
+}
