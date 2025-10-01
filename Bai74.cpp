3fd5e49c04aaec24f7d6af3d29b05732e57d8c0d
SuperCells <75522234+huulong1412@users.noreply.github.com>
2025-10-01 13:34:29 +0700

    Add files via upload

---
+++ Bai74.cpp
@@ -0,0 +1,41 @@
+/* Bài 74: Tính S(x, n) = 1 – x + x^3/3! – x^5/5! + … + (-1)^n+1 * x^2n+1/(2n + 1)! */
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int i, n;
+    float x, S, T;
+    long M, N;
+    printf("\nNhap x: ");
+    scanf("%f", &x);
+    do
+    {
+    printf("\nNhap n(n >= 1) : ");
+    scanf("%d", &n);
+      if(n < 1)
+      {
+          printf("\n N phai >= 1. Xin nhap lai !");
+      }
+
+    }while(n < 1);
+
+    S = 1;
+    N = 1;
+    i = 1;
+
+    while(i <= n)
+    {
+        
+        T = pow(x, (2 * i + 1));
+        M = i * 2 + 1;
+        N = N * M * (M - 1);
+        S = S + pow(-1, (float)(i + 1)) * T/N ;
+        i++;
+    }
+    printf("\nTong la %f", S - x);
+    getch();
+    return 0;
+}				
