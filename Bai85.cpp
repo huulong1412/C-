dff0148b27954ced3e926563d5b49542b9332394
SuperCells <75522234+huulong1412@users.noreply.github.com>
2025-10-06 16:53:10 +0700

    Add files via upload

---
+++ Bai85.cpp
@@ -0,0 +1,36 @@
+/* Bài 85: Nhập vào tháng của 1 năm. Cho biết tháng thuộc quý mấy trong năm */
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int thang;
+    printf("\nNhap thang: ");
+    scanf("%d", &thang);
+    switch (thang)
+    {
+    case 1:
+    case 2:
+    case 3:
+        printf("\nQui mot");
+        break;
+    case 4:
+    case 5:
+    case 6:
+        printf("\nQui hai");
+        break;
+    case 7:
+    case 8:
+    case 9:
+        printf("\nQui ba");
+        break;
+    case 10:
+    case 11:
+    case 12:
+        printf("\nQui bon");
+        break;
+    }
+    getch();
+    return 0;
+
+}			
