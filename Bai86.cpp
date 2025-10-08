d088366623a8f2981f9a6601b4a73824ee801c6d
SuperCells <75522234+huulong1412@users.noreply.github.com>
2025-10-08 11:52:37 +0700

    Add files via upload

---
+++ Bai86.cpp
@@ -0,0 +1,21 @@
+/* Bài 86: Tính S(n) = 1^3 + 2^3 + … + N^3 */
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, n, S;
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    S = 0;
+    i = 1;
+    while(i <= n)
+    {
+        S = S + i * i * i;
+        i++;
+    }
+    printf("\nTong la %d", S);
+    getch();
+    return 0;
+}						
